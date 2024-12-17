978e999e09b56886792e95a617e3b6cfeae31d23
Dominiksu <suchdo99@gmail.com>
2024-12-17 17:28:46 +0100

    Ukol_08 doplnění

--- Ukol_8/ukol_08.py
+++ Ukol_8/ukol_08.py
@@ -1,8 +1,17 @@
 import re
 
 def check_phonenumber(phone_number):
-    rx_number = re.compile(r'^\+?[\d\s]+$')
+    rx_number = re.compile(r'^(\+?\d{3})? ? ?\d{3} ?\d{3} ?\d{3}$')
     phone_check = rx_number.search(phone_number)
     return bool (phone_check)
 
-print(check_phonenumber('+ 444433555'))
+print(check_phonenumber('777777777'))
+print(check_phonenumber('777 777 777'))
+print(check_phonenumber('+420777777777'))
+print(check_phonenumber('+420 777 777 777'))
+print(check_phonenumber('608 192 292'))
+print(check_phonenumber('608 192 292...'))
+print(check_phonenumber('test'))
+print(check_phonenumber('+420'))
+print(check_phonenumber('+420 '))
+print(check_phonenumber('777777777666666666666666666'))
